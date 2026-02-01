ee49a831762d0fe6bd4089c90b45cb221e219062
ayazirshad <ayazhajana180@gmail.com>
2026-02-01 23:46:23 +0500

    fixed changes

--- js/tr.js
+++ js/tr.js
@@ -4,17 +4,17 @@
   function setLanguage(lang) {
     localStorage.setItem(LANG_KEY, lang);
     const currentPath = window.location.pathname;
-    const isUnderTR = currentPath.includes("/TR/");
+    const isUnderTR = currentPath.includes("/tr/");
     const fileName = currentPath.split("/").pop() || "index.html";
 
     if (lang === "tr") {
       if (!isUnderTR) {
-        // Moving from root to TR
-        window.location.href = "TR/" + fileName;
+        // Moving from root to tr
+        window.location.href = "tr/" + fileName;
       }
     } else {
       if (isUnderTR) {
-        // Moving from TR to root
+        // Moving from tr to root
         window.location.href = "../" + fileName;
       }
     }
@@ -23,11 +23,11 @@
   function checkLanguage() {
     const preferredLang = localStorage.getItem(LANG_KEY);
     const currentPath = window.location.pathname;
-    const isUnderTR = currentPath.includes("/TR/");
+    const isUnderTR = currentPath.includes("/tr/");
     const fileName = currentPath.split("/").pop() || "index.html";
 
     if (preferredLang === "tr" && !isUnderTR) {
-      window.location.href = "TR/" + fileName;
+      window.location.href = "tr/" + fileName;
     } else if (preferredLang === "en" && isUnderTR) {
       window.location.href = "../" + fileName;
     }
