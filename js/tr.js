662ded543d6f6d5498b667dd929eda5bf33ec2b2
ayazirshad <ayazhajana180@gmail.com>
2026-02-02 13:16:53 +0500

    updated assets of tr

--- js/tr.js
+++ js/tr.js
@@ -1,11 +1,22 @@
 (function () {
   const LANG_KEY = "preferredLanguage";
 
+  const TRfiles = [
+    "ai_interviewer.html",
+    "demo_page_1.html",
+    "index.html",
+    "midsize_businesses.html",
+  ];
+
   function setLanguage(lang) {
     localStorage.setItem(LANG_KEY, lang);
     const currentPath = window.location.pathname;
     const isUnderTR = currentPath.includes("/tr/");
-    const fileName = currentPath.split("/").pop() || "index.html";
+    const fileName = TRfiles.includes(currentPath.split("/").pop())
+      ? currentPath.split("/").pop()
+      : "index.html";
+    console.log("filename", fileName);
+    console.log("current path", currentPath);
 
     if (lang === "tr") {
       if (!isUnderTR) {
@@ -25,8 +36,9 @@
     const currentPath = window.location.pathname;
     const isUnderTR = currentPath.includes("/tr/");
     const fileName = currentPath.split("/").pop() || "index.html";
+    const hasTranslation = TRfiles.includes(fileName);
 
-    if (preferredLang === "tr" && !isUnderTR) {
+    if (preferredLang === "tr" && !isUnderTR && hasTranslation) {
       window.location.href = "tr/" + fileName;
     } else if (preferredLang === "en" && isUnderTR) {
       window.location.href = "../" + fileName;
@@ -34,17 +46,39 @@
   }
 
   document.addEventListener("DOMContentLoaded", () => {
-    const langButtons = document.querySelectorAll(".choose-lang-button");
-    langButtons.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        const langText = btn.textContent.trim().toLowerCase();
-        if (langText === "türkçe") {
-          setLanguage("tr");
-        } else if (langText === "english") {
-          setLanguage("en");
-        }
-      });
+    // const langButtons = document.querySelectorAll(".choose-lang-button");
+    const langEng = document.getElementById("lang-eng");
+    const langTr = document.getElementById("lang-tr");
+    const langEngM = document.getElementById("lang-eng-m");
+    const langTrM = document.getElementById("lang-tr-m");
+
+    langEng.addEventListener("click", () => {
+      setLanguage("en");
     });
+
+    langTr.addEventListener("click", () => {
+      console.log("tr btn clicked");
+      setLanguage("tr");
+    });
+
+    langEngM.addEventListener("click", () => {
+      setLanguage("en");
+    });
+
+    langTrM.addEventListener("click", () => {
+      setLanguage("tr");
+    });
+
+    // langButtons.forEach((btn) => {
+    //   btn.addEventListener("click", () => {
+    //     const langText = btn.textContent.trim().toLowerCase();
+    //     if (langText === "türkçe") {
+    //       setLanguage("tr");
+    //     } else if (langText === "english") {
+    //       setLanguage("en");
+    //     }
+    //   });
+    // });
   });
 
   // Run immediately to handle auto-redirect on script load or refresh
